c1415d955e71ba70729c4f4a686c374a308869a7
Joshua Sahagun <joshuasahagun1996@yahoo.com>
2023-09-11 16:18:21 -0700

    finalized plotting of scatter plot

--- repo_mining/Joshua_scatterplot.py
+++ repo_mining/Joshua_scatterplot.py
@@ -6,8 +6,16 @@ import random
 
 def createNewColor(authorName, authorColorDict):
 	if authorName not in authorColorDict:
-		authorColorDict[authorName] =  [random.randint(0,255) , random.randint(0,255) , random.randint(0,255)]
-		
+		authorColorDict[authorName] =  [random.random() , random.random() , random.random()]
+
+def trackFileName(fileName, fileNameList):
+	if fileName not in fileNameList:
+		fileNameList.append(fileName)
+
+def trackDate(listedDate, trackedDates):
+	if listedDate not in trackedDates:
+		trackedDates.append(listedDate)
+
 def scatterPlot(reader):
 	next(reader)
 	next(reader)
@@ -23,6 +31,13 @@ def scatterPlot(reader):
 		dateWithoutTime = re.sub("T.*", '', unproccessedDate)
 		
 		createNewColor(authorName, authorColorDictionary)
+		trackFileName(fileName, fileNameList)
+		trackDate(dateWithoutTime, dateList)
+		plot.scatter(
+			fileNameList.index(fileName),
+			dateList.index(dateWithoutTime),
+			color=authorColorDictionary[authorName]
+			)
 		
 		# dateYear = dateWithoutTime[0:4]
 		# dateMonth = dateWithoutTime[5:7]
@@ -32,6 +47,10 @@ def scatterPlot(reader):
 		# pointList = [authorName, fileName, dateWithoutTime]
 		# print(authorName + fileName + dateWithoutTime)
 		# print(authorName +  fileName + dateYear + dateMonth + dateDay)
+	plot.xlabel('files')
+	plot.ylabel('weeks')
+	plot.show()
+
 
 repo = 'scottyab/rootbeer'
 # repo = 'Skyscanner/backpack' # This repo is commit heavy. It takes long to finish executing
@@ -45,5 +64,7 @@ fileCSV = open(fileInput, 'r')
 reader = csv.reader(fileCSV)
 
 authorColorDictionary = dict()
+fileNameList = []
+dateList = []
 
 scatterPlot(reader)
