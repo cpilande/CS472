19db9aebd33716d3e535d392c21da40cb4b882e1
Joshua Sahagun <joshuasahagun1996@yahoo.com>
2023-09-11 15:41:46 -0700

    updated to create new color for each unique author

--- repo_mining/Joshua_scatterplot.py
+++ repo_mining/Joshua_scatterplot.py
@@ -1,10 +1,17 @@
 import csv
 import regex as re
 import matplotlib.pyplot as plot
+import random
 
+
+def createNewColor(authorName, authorColorDict):
+	if authorName not in authorColorDict:
+		authorColorDict[authorName] =  [random.randint(0,255) , random.randint(0,255) , random.randint(0,255)]
+		
 def scatterPlot(reader):
 	next(reader)
 	next(reader)
+
 	for row in reader:
 		unproccessedName = row[0]
 		unproccessedDate = row[1]
@@ -14,17 +21,17 @@ def scatterPlot(reader):
 		fileName = re.sub(".*:\s" , '', nameCompounded)
 
 		dateWithoutTime = re.sub("T.*", '', unproccessedDate)
-
-		dateYear = re.sub("\-.*","",dateWithoutTime)
-		# dateMonth = re.sub("","",dateYear)
-		# dateDay = re.sub("","",dateMonth)
+		
+		createNewColor(authorName, authorColorDictionary)
+		
+		# dateYear = dateWithoutTime[0:4]
+		# dateMonth = dateWithoutTime[5:7]
+		# dateDay = dateWithoutTime[8:11]
 		next(reader)
 
 		# pointList = [authorName, fileName, dateWithoutTime]
-		print(authorName + "\n" + fileName + "\n" + dateYear)
-
-
-		
+		# print(authorName + fileName + dateWithoutTime)
+		# print(authorName +  fileName + dateYear + dateMonth + dateDay)
 
 repo = 'scottyab/rootbeer'
 # repo = 'Skyscanner/backpack' # This repo is commit heavy. It takes long to finish executing
@@ -36,5 +43,7 @@ file = repo.split('/')[1]
 fileInput = 'data/file_' + file + 'author' + '.csv'
 fileCSV = open(fileInput, 'r')
 reader = csv.reader(fileCSV)
+
+authorColorDictionary = dict()
 
 scatterPlot(reader)
